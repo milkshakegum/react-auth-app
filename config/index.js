a947bba4b1a999ae6f041bc152d66b0f59b6de03
Muhammad Musa <musaghauri@hotmail.com>
2018-01-17 19:10:28 +0500

    added default for MAILGUN_FLAG

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ const environment = {
 const prod = process.env.NODE_ENV === 'production';
 const config = {
     API_HOST: !!prod ? process.env.HOST : `http://localhost:${parseInt(process.env.PORT, 10)||3000}`,
-    MAILGUN_FLAG: process.env.MAILGUN_FLAG,
+    MAILGUN_FLAG: process.env.MAILGUN_FLAG||'false',
     bucket: {
         slug: process.env.COSMIC_BUCKET,
         read_key: process.env.COSMIC_READ_KEY,
